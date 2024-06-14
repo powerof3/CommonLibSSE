5cdde1d2ca3300f3d8e42b7e02e33fca7dfe211d
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-06-14 11:29:43 +0530

    update id for `InventoryChanges::RemoveAllItems`

--- src/RE/I/InventoryChanges.cpp
+++ src/RE/I/InventoryChanges.cpp
@@ -65,7 +65,7 @@ namespace RE
 	void InventoryChanges::RemoveAllItems(TESObjectREFR* a_ref, TESObjectREFR* a_moveToRef, bool a_arg4, bool a_keepOwnership, bool a_arg6)
 	{
 		using func_t = decltype(&InventoryChanges::RemoveAllItems);
-		REL::Relocation<func_t> func{ RELOCATION_ID(15878, 16118) };
+		REL::Relocation<func_t> func{ RELOCATION_ID(15878, 441567) };
 		return func(this, a_ref, a_moveToRef, a_arg4, a_keepOwnership, a_arg6);
 	}
 
