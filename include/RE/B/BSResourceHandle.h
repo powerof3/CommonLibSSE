1a7b1cf6d0ceec1ca1fdb77fef87b9ba7d66091a
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-08-18 20:04:38 +0530

    Add `BSResource::RHandleType` helpers

--- include/RE/B/BSResourceHandle.h
+++ include/RE/B/BSResourceHandle.h
@@ -21,8 +21,31 @@ namespace RE
 			using U_Entry = T_Entry;
 			using U_EntryDB = T_EntryDB;
 
+			[[nodiscard]] constexpr T_Entry* get() const noexcept
+			{
+				return _entry;
+			}
+
+			[[nodiscard]] explicit constexpr operator bool() const noexcept
+			{
+				return static_cast<bool>(_entry);
+			}
+
+			[[nodiscard]] constexpr T_Entry& operator*() const noexcept
+			{
+				assert(static_cast<bool>(*this));
+				return *_entry;
+			}
+
+			[[nodiscard]] constexpr T_Entry* operator->() const noexcept
+			{
+				assert(static_cast<bool>(*this));
+				return _entry;
+			}
+
+		protected:
 			// members
-			T_Entry* entry;  // 00
+			T_Entry* _entry;  // 00
 		};
 	}
 
