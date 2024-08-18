dc2177a2f779dcbfcba573bc5ceb45020560bf36
powerof3 <32599957+powerof3@users.noreply.github.com>
2024-08-18 12:44:54 +0530

    Add `ModelDBHandle`, `BSResource` classes

---
+++ include/RE/B/BSResourceHandle.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "RE/N/NiSmartPointer.h"
+#include "RE/B/BSResourceEntryDB.h"
+
+namespace RE
+{
+	class NiNode;
+
+	namespace BSModelDB
+	{
+		struct DBTraits;
+	}
+
+	namespace BSResource
+	{
+		template <class T_Entry, class T_EntryDB>
+		class RHandleType
+		{
+		public:
+			using U_Entry = T_Entry;
+			using U_EntryDB = T_EntryDB;
+
+			// members
+			T_Entry* entry;  // 00
+		};
+	}
+
+	using ModelDBHandle = BSResource::RHandleType<BSResource::Entry<NiPointer<NiNode>, BSResource::EntryDBTraits<BSModelDB::DBTraits, BSResource::EntryDB<BSModelDB::DBTraits>>::CArgs>, BSResource::EntryDB<BSModelDB::DBTraits>>;
+}
