b19131697b9ffaa6e9c12f0b9df00349d50323e8
FlayaN <hannes.feldt@gmail.com>
2024-08-20 00:24:20 +0200

    fix: build and follow sse naming standard

--- include/RE/B/BSComputeShader.h
+++ include/RE/B/BSComputeShader.h
@@ -15,16 +15,14 @@ namespace RE
 		~BSComputeShader() override;  // 00
 
 		// override (BSReloadShaderI)
-		void ReloadShaders(void* stream) override;  // 00
+		void ReloadShaders(void* a_stream) override;  // 00
 
 		// add
-		virtual void Dispatch(uint32_t techniqueId, uint32_t threadGroupCountX, uint32_t threadGroupCountY, uint32_t threadGroupCountZ);  // 02
+		virtual void Dispatch(uint32_t a_techniqueId, uint32_t a_threadGroupCountX, uint32_t a_threadGroupCountY, uint32_t a_threadGroupCountZ);  // 02
 
+		// members
 		const char*                                      name;        // 18
 		BSTHashMap<uint32_t, BSGraphics::ComputeShader*> shadersMap;  // 20
-
-	private:
-		KEEP_FOR_RE()
 	};
 	static_assert(sizeof(BSComputeShader) == 0x50);
 }
