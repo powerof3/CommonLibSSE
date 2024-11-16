719970554d00f5839c361e5eb100acbbe913b275
Parapets <southwest85b@gmail.com>
2024-11-16 01:39:07 +0000

    Add BSTList

--- cmake/sourcelist.cmake
+++ cmake/sourcelist.cmake
@@ -373,6 +373,7 @@ set(SOURCES
 	include/RE/B/BSTEvent.h
 	include/RE/B/BSTFreeList.h
 	include/RE/B/BSTHashMap.h
+	include/RE/B/BSTList.h
 	include/RE/B/BSTLocklessQueue.h
 	include/RE/B/BSTMessageQueue.h
 	include/RE/B/BSTObjectDictionary.h
