cf6f900d4a43a3098b949675bd623e6d9e364782
Fuzzles <fuzzmyrim@gmail.com>
2025-09-27 13:43:45 -0700

    Add TutorialMenu::OpenTutorialMenu

--- include/RE/Offsets.h
+++ include/RE/Offsets.h
@@ -1281,6 +1281,11 @@ namespace RE
 			inline constexpr REL::ID ResetQuest(static_cast<std::uint64_t>(24486));
 		}
 
+		namespace TutorialMenu
+		{
+			inline constexpr REL::ID TutorialsShownA(static_cast<std::uint64_t>(406588));
+		}
+
 		namespace UI
 		{
 			inline constexpr REL::ID Singleton(static_cast<std::uint64_t>(514178));
