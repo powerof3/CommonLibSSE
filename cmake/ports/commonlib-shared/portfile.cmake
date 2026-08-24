e777bd828814cdee0efd1beedf0c58a36e732d0c
powerof3 <powerof3@users.noreply.github.com>
2026-08-24 10:36:21 +0530

    Update `commonlib-shared`, add `xoshiro-cpp` overlay port

--- cmake/ports/commonlib-shared/portfile.cmake
+++ cmake/ports/commonlib-shared/portfile.cmake
@@ -3,17 +3,19 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libxse/commonlib-shared
-    REF bbd60153e4b5ecb4b7949513a1af4df43dc6e267
-    SHA512 cb490e70843297f4a4840c1f556e17eecb3171d106e508e2b48164b7bebf0fcb5b05d6f91c6441c9efa409d185da65833bb5ba10d42ef407bb845f6a647aab98
-    HEAD_REF support/cmake
+    REF 0e0796f81e61e4ef4c5110f9b64ca058eb0eb459
+    SHA512 e18c5f2d0755f47a11c903a5c0aeff4b31510fa3bbeb752bbd64ba708b235d8d1e12b3d92c8cc3adce487d792a0ce362d935685240c1386d61826ccd66f330f3
+    HEAD_REF main
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
     FEATURES
-        ini   COMMONLIB_INI
-        json  COMMONLIB_JSON
-        toml  COMMONLIB_TOML
-        xbyak COMMONLIB_XBYAK
+        ini    COMMONLIB_INI
+        json   COMMONLIB_JSON
+		random COMMONLIB_RANDOM
+        toml   COMMONLIB_TOML
+        xbyak  COMMONLIB_XBYAK
+
 )
 
 vcpkg_cmake_configure(
