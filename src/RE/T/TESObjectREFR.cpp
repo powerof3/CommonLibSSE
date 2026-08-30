c597458456dc36c1e3c7afedd07458f8c49cef02
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2026-08-30 02:29:17 -0600

    feat: `TESObjectREFR::AddLockChange`

--- src/RE/T/TESObjectREFR.cpp
+++ src/RE/T/TESObjectREFR.cpp
@@ -72,6 +72,13 @@ namespace RE
 		return func(this);
 	}
 
+	void TESObjectREFR::AddLockChange()
+	{
+		using func_t = decltype(&TESObjectREFR::AddLockChange);
+		static REL::Relocation<func_t> func{ REL::ID(19512) };
+		return func(this);
+	}
+
 	DoorTeleportData* TESObjectREFR::AddTeleport()
 	{
 		using func_t = decltype(&TESObjectREFR::AddTeleport);
