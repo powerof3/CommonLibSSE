67872501455efc70fc17967332015561bd184a3a
SkyHorizon3 <neuner.r@gmx.de>
2026-09-03 19:48:40 +0200

    fix: BSInputEventQueue

--- include/RE/B/BSInputEventQueue.h
+++ include/RE/B/BSInputEventQueue.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include "RE/A/AmiiboEvent.h"
 #include "RE/B/BSTSingleton.h"
 #include "RE/B/ButtonEvent.h"
 #include "RE/C/CharEvent.h"
 #include "RE/D/DeviceConnectEvent.h"
 #include "RE/K/KinectEvent.h"
+#include "RE/M/MotionGestureEvent.h"
 #include "RE/M/MouseMoveEvent.h"
+#include "RE/S/SixaxisEvent.h"
 #include "RE/T/ThumbstickEvent.h"
 
 namespace RE
@@ -19,6 +22,11 @@ namespace RE
 		inline static constexpr std::uint8_t MAX_THUMBSTICK_EVENTS = 2;
 		inline static constexpr std::uint8_t MAX_CONNECT_EVENTS = 1;
 		inline static constexpr std::uint8_t MAX_KINECT_EVENTS = 1;
+#ifdef SKYRIM_SUPPORT_AE
+		inline static constexpr std::uint8_t MAX_SIXAXIS_EVENTS = 2;
+		inline static constexpr std::uint8_t MAX_GESTURE_EVENTS = 2;
+		inline static constexpr std::uint8_t MAX_AMIIBO_EVENTS = 1;
+#endif
 
 		static BSInputEventQueue* GetSingleton();
 
@@ -62,23 +70,32 @@ namespace RE
 		void ClearInputQueue();
 
 		// members
-		std::uint8_t       pad001;                                   // 001
-		std::uint16_t      pad002;                                   // 002
-		std::uint32_t      buttonEventCount;                         // 004
-		std::uint32_t      charEventCount;                           // 008
-		std::uint32_t      mouseEventCount;                          // 00C
-		std::uint32_t      thumbstickEventCount;                     // 010
-		std::uint32_t      connectEventCount;                        // 014
-		std::uint32_t      kinectEventCount;                         // 018
-		std::uint32_t      pad01C;                                   // 01C
+		std::uint8_t  pad001;                // 001
+		std::uint16_t pad002;                // 002
+		std::uint32_t buttonEventCount;      // 004
+		std::uint32_t charEventCount;        // 008
+		std::uint32_t mouseEventCount;       // 00C
+		std::uint32_t thumbstickEventCount;  // 010
+		std::uint32_t connectEventCount;     // 014
+		std::uint32_t kinectEventCount;      // 018
+#ifdef SKYRIM_SUPPORT_AE
+		std::uint32_t sixaxisEventCount;  // 01C
+		std::uint32_t gestureEventCount;  // 020
+		std::uint32_t amiiboEventCount;   // 024
+#endif
 		ButtonEvent        buttonEvents[MAX_BUTTON_EVENTS];          // 020
 		CharEvent          charEvents[MAX_CHAR_EVENTS];              // 200
 		MouseMoveEvent     mouseEvents[MAX_MOUSE_EVENTS];            // 2A0
 		ThumbstickEvent    thumbstickEvents[MAX_THUMBSTICK_EVENTS];  // 2D0
 		DeviceConnectEvent connectEvents[MAX_CONNECT_EVENTS];        // 330
 		KinectEvent        kinectEvents[MAX_KINECT_EVENTS];          // 350
-		InputEvent*        queueHead;                                // 380
-		InputEvent*        queueTail;                                // 388
+#ifdef SKYRIM_SUPPORT_AE
+		SixaxisEvent       sixaxisEvents[MAX_SIXAXIS_EVENTS];  // 388
+		MotionGestureEvent gestureEvents[MAX_GESTURE_EVENTS];  // 4A8
+		AmiiboEvent        amiiboEvents[MAX_AMIIBO_EVENTS];    // 518
+#endif
+		InputEvent* queueHead;  // 380
+		InputEvent* queueTail;  // 388
 
 	private:
 		template <class T>
@@ -97,5 +114,9 @@ namespace RE
 			}
 		}
 	};
+#ifdef SKYRIM_SUPPORT_AE
+	static_assert(sizeof(BSInputEventQueue) == 0x568);
+#else
 	static_assert(sizeof(BSInputEventQueue) == 0x390);
+#endif
 }
