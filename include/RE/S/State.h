4c85b767264bf042c90316a021c23b7a8539cdb0
powerof3 <powerof3@users.noreply.github.com>
2026-09-04 12:35:30 +0530

    Update `BSGraphics::State`

--- include/RE/S/State.h
+++ include/RE/S/State.h
@@ -39,47 +39,76 @@ namespace RE
 			std::uint32_t              unk038;                             // 038
 			std::uint32_t              unk03C;                             // 03C
 			std::uint32_t              unk040;                             // 040
-			float                      unk044;                             // 044
-			float                      unk048;                             // 048
-			std::uint32_t              frameCount;                         // 04C
-			bool                       unk50;                              // 050 - previously misnamed insideFrame
-			bool                       letterbox;                          // 051
-			bool                       unk052;                             // 052
-			bool                       compiledShaderThisFrame;            // 053
-			bool                       insideFrame;                        // 054 - previously misnamed useEarlyZ
-			bool                       unk055;                             // 055
+			float                      projectionPosScaleX;                // 044
+			float                      projectionPosScaleY;                // 048
 #ifdef SKYRIM_SUPPORT_AE
-			bool unk56;                                       // 056
-			bool doubleDynamicResolutionAdjustmentFrequency;  // 057
-			bool unk58;                                       // 058
-			bool unk59;                                       // 059
+			float         projectionPosScaleXUI;    // 04C
+			float         projectionPosScaleYUI;    // 050
+			std::uint32_t frameCount;               // 054
+			std::uint32_t gUpdateCounter;           // 058
+			std::uint32_t unk05C;                   // 05C
+			bool          insideFrame;              // 060
+			bool          letterbox;                // 061
+			bool          unk062;                   // 062
+			bool          unk063;                   // 063
+			bool          compiledShaderThisFrame;  // 064
+			bool          useEarlyZ;                // 065
+			std::uint16_t unk066;                   // 066
+			bool          unk068;                   // 068
+#else
+			std::uint32_t frameCount;               // 04C
+			bool          insideFrame;              // 050
+			bool          letterbox;                // 051
+			bool          unk052;                   // 052
+			bool          unk053;                   // 053
+			bool          compiledShaderThisFrame;  // 054
+			bool          useEarlyZ;                // 055
 #endif
-			NiPointer<NiSourceTexture> defaultTextureBlack;                   // 060
-			NiPointer<NiSourceTexture> defaultTextureWhite;                   // 068
-			NiPointer<NiSourceTexture> defaultTextureGrey;                    // 070
-			NiPointer<NiSourceTexture> defaultHeightMap;                      // 078
-			NiPointer<NiSourceTexture> defaultReflectionCubeMap;              // 080
-			NiPointer<NiSourceTexture> defaultFaceDetailMap;                  // 088
-			NiPointer<NiSourceTexture> defaultTexEffectMap;                   // 090
-			NiPointer<NiSourceTexture> defaultTextureNormalMap;               // 098
-			NiPointer<NiSourceTexture> defaultTextureDitherNoiseMap;          // 0A0
-			BSTArray<void*>            cameraDataCache;                       // 0A8
-			std::uint32_t              unk0C0;                                // 0C0
-			float                      haltonSequence[2][8];                  // 0C4
-			float                      dynamicResolutionWidthRatio;           // 104
-			float                      dynamicResolutionHeightRatio;          // 108
-			float                      dynamicResolutionPreviousWidthRatio;   // 10C
-			float                      dynamicResolutionPreviousHeightRatio;  // 110
-			std::uint32_t              dynamicResolutionIncreaseFrameWaited;  // 114
-			volatile std::int32_t      dynamicResolutionLock;                 // 118
-			bool                       canIncreaseDynamicResolution;          // 11C
-			bool                       canDecreaseDynamicResolution;          // 11D
-			bool                       canChangeDynamicResolution;            // 11E
+			NiPointer<NiSourceTexture> defaultTextureBlack;                   // 070
+			NiPointer<NiSourceTexture> defaultTextureWhite;                   // 078
+			NiPointer<NiSourceTexture> defaultTextureGrey;                    // 080
+			NiPointer<NiSourceTexture> defaultHeightMap;                      // 088
+			NiPointer<NiSourceTexture> defaultReflectionCubeMap;              // 090
+			NiPointer<NiSourceTexture> defaultFaceDetailMap;                  // 098
+			NiPointer<NiSourceTexture> defaultTexEffectMap;                   // 0A0
+			NiPointer<NiSourceTexture> defaultTextureNormalMap;               // 0A8
+			NiPointer<NiSourceTexture> defaultTextureDitherNoiseMap;          // 0B0
+			BSTArray<void*>            cameraDataCache;                       // 0B8
+			std::uint32_t              unk0C0;                                // 0D0
+			float                      haltonSequence[2][8];                  // 0D4
+			float                      dynamicResolutionWidthRatio;           // 114
+			float                      dynamicResolutionHeightRatio;          // 118
+			float                      dynamicResolutionPreviousWidthRatio;   // 11C
+			float                      dynamicResolutionPreviousHeightRatio;  // 120
+			std::uint32_t              dynamicResolutionIncreaseFrameWaited;  // 124
+			volatile std::int32_t      dynamicResolutionLock;                 // 128
+			bool                       canIncreaseDynamicResolution;          // 12C
+			bool                       canDecreaseDynamicResolution;          // 12D
+			bool                       canChangeDynamicResolution;            // 12E
 		};
 #ifdef SKYRIM_SUPPORT_AE
-		static_assert(sizeof(State) == 0x120);
+		static_assert(sizeof(State) == 0x130);
+		static_assert(offsetof(State, frameCount) == 0x54);
+		static_assert(offsetof(State, insideFrame) == 0x60);
+		static_assert(offsetof(State, letterbox) == 0x61);
+		static_assert(offsetof(State, compiledShaderThisFrame) == 0x64);
+		static_assert(offsetof(State, useEarlyZ) == 0x65);
+		static_assert(offsetof(State, defaultTextureBlack) == 0x70);
+		static_assert(offsetof(State, defaultTextureWhite) == 0x78);
+		static_assert(offsetof(State, cameraDataCache) == 0xB8);
+		static_assert(offsetof(State, dynamicResolutionWidthRatio) == 0x114);
+		static_assert(offsetof(State, dynamicResolutionLock) == 0x128);
 #else
 		static_assert(sizeof(State) == 0x118);
+		static_assert(offsetof(State, frameCount) == 0x4C);
+		static_assert(offsetof(State, insideFrame) == 0x50);
+		static_assert(offsetof(State, letterbox) == 0x51);
+		static_assert(offsetof(State, compiledShaderThisFrame) == 0x54);
+		static_assert(offsetof(State, useEarlyZ) == 0x55);
+		static_assert(offsetof(State, defaultTextureBlack) == 0x58);
+		static_assert(offsetof(State, defaultTextureWhite) == 0x60);
+		static_assert(offsetof(State, cameraDataCache) == 0xA0);
+		static_assert(offsetof(State, dynamicResolutionWidthRatio) == 0xFC);
+		static_assert(offsetof(State, dynamicResolutionLock) == 0x110);
 #endif
 	}
-}
